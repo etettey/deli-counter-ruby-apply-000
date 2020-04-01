2a0bfc913ea57049ea51eb7126a483b2b82c8776
resourceful-role-9993 <unicetee@gmail.com>
2020-04-01 13:58:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,15 @@
 # Write your code here.
 
-def line(names)
-  if names.empty?
-puts "The line is currently empty."
-else
-  current_line="The line is currently:"
-  names.each.with_index(1) do |name,i|
-  current_line+= " #{i}. #{name}"
+def line(customer_line)
+ i=1
+  while i < customer_line.length
+    print "The line is currently:  #{i}. #{customer_line[i]},"
+    i+=1
 end
-puts current_line
+if customer_line.empty?
+  puts "The line is currently empty."
 end
 end
-
 def take_a_number (customer_line,new_customer)
   customer_line << new_customer
   puts "Welcome, #{new_customer}. You are number #{customer_line.length} in line."
